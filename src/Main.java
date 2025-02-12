d14968eec5605fdf1332d0f0fd0eef77763c2f0d
Lyakh-Sergey <me@ljah.ru>
2025-02-12 18:36:55 +0300

    Update src/Main.java
    Co-authored-by: Ilya Savinov <il.savinov@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 1");
-        for (int i = 1; i <= 10; i = i + 1) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
         System.out.println("Задача 2");
